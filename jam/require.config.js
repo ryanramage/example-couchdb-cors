1dd38be612c559825efb71285dd0130a01f92e9c
Ryan Ramage <ryan.ramage@gmail.com>
2013-05-15 21:20:31 -0600

    first commit

---
+++ jam/require.config.js
@@ -0,0 +1,70 @@
+var jam = {
+    "packages": [
+        {
+            "name": "couchr",
+            "location": "jam/couchr",
+            "main": "couchr-browser.js"
+        },
+        {
+            "name": "events",
+            "location": "jam/events",
+            "main": "events.js"
+        },
+        {
+            "name": "jquery",
+            "location": "jam/jquery",
+            "main": "dist/jquery.js"
+        }
+    ],
+    "version": "0.2.17",
+    "shim": {}
+};
+
+if (typeof require !== "undefined" && require.config) {
+    require.config({
+    "packages": [
+        {
+            "name": "couchr",
+            "location": "jam/couchr",
+            "main": "couchr-browser.js"
+        },
+        {
+            "name": "events",
+            "location": "jam/events",
+            "main": "events.js"
+        },
+        {
+            "name": "jquery",
+            "location": "jam/jquery",
+            "main": "dist/jquery.js"
+        }
+    ],
+    "shim": {}
+});
+}
+else {
+    var require = {
+    "packages": [
+        {
+            "name": "couchr",
+            "location": "jam/couchr",
+            "main": "couchr-browser.js"
+        },
+        {
+            "name": "events",
+            "location": "jam/events",
+            "main": "events.js"
+        },
+        {
+            "name": "jquery",
+            "location": "jam/jquery",
+            "main": "dist/jquery.js"
+        }
+    ],
+    "shim": {}
+};
+}
+
+if (typeof exports !== "undefined" && typeof module !== "undefined") {
+    module.exports = jam;
+}
